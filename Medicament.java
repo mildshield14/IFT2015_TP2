1bc7c928d8e49053702e07c7b12a0bcb8abbb12e
Vennila Sooben <80240232+mildshield14@users.noreply.github.com>
2023-07-04 22:16:04 +0000

    new files added

--- Medicament.java
+++ Medicament.java
@@ -1,1 +1,12 @@
+import java.util.UUID;
+import java.time.LocalDate;
 
+class Medicament{
+    private String nom;
+    private UUID uuid;
+    private LocalDate dateExpi;
+
+   public Medicament(String nom, ){
+
+   }
+}
