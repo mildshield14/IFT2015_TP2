7e193bf637b087fd0a67768b8e77980cd0e75c02
marionnee <114243727+marionnee@users.noreply.github.com>
2023-07-04 20:50:25 -0400

    Update ClientPrescription.java

--- ClientPrescription.java
+++ ClientPrescription.java
@@ -9,7 +9,11 @@ class ClientPrescription{
 
     public Client(int repetition, ArrayList<Medicament> medicaments, UUID idClient) {
         this.repetition = repetition;
+        this.medicaments = medicaments;
+        this.idClient = idClient;
     }
 
-    
+    public int getRepetition() { return repetition; }
+    public ArrayList<Medicament> getMedicaments() { return repetition; }
+    public UUID getIdClient() { return idClient; }
 }
