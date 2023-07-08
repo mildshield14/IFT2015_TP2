fd7fac635271c31ae6bd31579b6cd2384ff6e148
marionnee <114243727+marionnee@users.noreply.github.com>
2023-07-08 00:04:25 -0400

    Update ClientPrescription.java

--- ClientPrescription.java
+++ ClientPrescription.java
@@ -8,7 +8,9 @@ class ClientPrescription{
     private ArrayList<Medicament> medicaments;
     private UUID idClient;
     private LocalDate dateObtenue;
-    private LocalDate currentDate;
+    private static BST tree = new BST();
+    private static LocalDate currentDate;
+
 
     public ClientPrescription(int repetition, ArrayList<Medicament> medicaments, UUID idClient) {
         this.repetition = repetition;
@@ -25,85 +27,44 @@ class ClientPrescription{
         this.currentDate=dateCurrent;
     }
 
-
     public  static String methodPrescription(String line, LocalDate date){
 // TODO; change to \t
-        String[] parts = line.split(" ");
+        String[] parts = line.split("\\s+");
 
-        
-        int num1 = Integer.parseInt(parts[1]);  
-        int num2 = Integer.parseInt(parts[2]); 
-        String med = parts[0];  
+
+        int num1 = Integer.parseInt(parts[1]);
+        int num2 = Integer.parseInt(parts[2]);
+        String med = parts[0];
 
         int total = num1*num2;
-      
+
         Medicament foundMed = BST.findClosest(med,date);
 
         String outputstring="";
 
-       if (foundMed !=null && foundMed.getStock()>=total){
-            
+        if (foundMed !=null && foundMed.getStock()>=total){
+
             BST.removeMed(foundMed);
             foundMed.setStock(foundMed.getStock()-total);
             BST.addMed(foundMed);
             outputstring=(med + "\t" + num1 + "\t"+ num2 +"\t"+ "OK");
-        }else if (!((foundMed.getStock()>=total))){
+        }else if (foundMed != null && !(foundMed.getStock()>=total)){
             outputstring=(med + "\t" + num1 + "\t"+ num2 +"\t"+ "COMMANDE");
-        } else{
-            System.out.println("Med does not exists??");
+        } else if (foundMed == null){
+            outputstring=(med + "\t" + num1 + "\t"+ num2 +"\t"+ "COMMANDE");
         }
 
 
         return outputstring;
 
-               }
+    }
 
-               public static void main(String[] args) {
-             //   BST t = new BST();
-    
+    public static void main(String[] args) {
+
 // utiliser this.tree pour utiliser les methodes
-// utiliset outputStock quand la ligne est Stock
-Medicament m1 = new Medicament("Med1", UUID.randomUUID(), LocalDate.of(2023, 7, 1),100);
-Medicament m2 = new Medicament("Med2", UUID.randomUUID(), LocalDate.of(2023, 7, 2),100);
-Medicament m3 = new Medicament("Med3", UUID.randomUUID(), LocalDate.of(2023, 7, 3),100);
-Medicament m4 = new Medicament("Med4", UUID.randomUUID(), LocalDate.of(2023, 7, 4),200);
-Medicament m5 = new Medicament("Med5", UUID.randomUUID(), LocalDate.of(2023, 7, 5),100);
-Medicament m6 = new Medicament("Med4", UUID.randomUUID(), LocalDate.of(2023, 7, 5),350);
-Medicament m7 = new Medicament("Med4", UUID.randomUUID(), LocalDate.of(2023, 7, 10),450);
-
-BST.addMed(m3);
-
-BST.addMed(m2);
-
-BST.addMed(m1);
-
-BST.addMed(m4);
-
-BST.addMed(m7);
-
-BST.addMed(m6);
-
-BST.addMed(m5);
-
-System.out.println(methodPrescription("Med4 50 20" , LocalDate.of(2024,12,12)));
-
-BST.outputStock(LocalDate.of(2022,12,12));
-
-System.out.println("\n");
-
-BST.removeMed(m1);
+// utiliset outputStock quand la ligne est Stock;
 
 
-BST.outputStock(LocalDate.of(2022,12,12));
+    }
 
-System.out.println("\n");
-
-BST.removeAllExpired( LocalDate.of(2023,7,3));
-
-BST.outputStock(LocalDate.of(2022,12,12));
-
-
-
-               }
-  
 }
